dc215a7ee48ff4aca40a17a2a7547663e95959c2
Denis Kostiv <deniskostiv@gmail.com>
2023-06-08 16:08:02 +0300

    add favicon and paginator

--- fisher_news/posts/views.py
+++ fisher_news/posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:7]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 7)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     group = Group.objects.all()
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
         'groups': group,
     }
     return render(request, 'posts/index.html', context)
