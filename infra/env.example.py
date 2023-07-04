73fbd3cabff6abab51742ee75f4ecdcc914e3f5a
Denis Kostiv <deniskostiv@gmail.com>
2023-07-04 15:24:16 +0300

    add nginx to docker conpose and use DockerHub

---
+++ infra/env.example.py
@@ -0,0 +1,24 @@
+# BOTS TOKENs
+TOKEN =
+ADMIN_TOKEN = 
+CHAT_TOKEN = 
+
+# Site
+URL = 
+
+# Django
+SECRET_KEY = 
+
+# telegram chat
+MY_CHAT = 
+
+# OpenAi token
+AI_TOKEN =
+
+# postgresql
+DB_ENGINE = 
+DB_NAME = 
+POSTGRES_USER = 
+POSTGRES_PASSWORD = 
+HOST = 
+PORT = 
