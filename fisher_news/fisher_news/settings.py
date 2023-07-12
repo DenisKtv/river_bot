33067b710abad0caa3ae4887ed8d476c369a6458
Denis Kostiv <deniskostiv@gmail.com>
2023-07-12 15:32:41 +0300

    add proxy proto

--- fisher_news/fisher_news/settings.py
+++ fisher_news/fisher_news/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-0p5dasdafafref')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['185.107.237.87', 'fisherjournal.xyz']
+ALLOWED_HOSTS = ['185.107.237.87', 'fisherjournal.xyz', 'www.fisherjournal.xyz']
 
 INSTALLED_APPS = [
     'sorl.thumbnail',
